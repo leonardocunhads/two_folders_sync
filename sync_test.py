ac1e04b618b31ec177397d54395fb1142c221625
Leonardo Cunha <leonardocunhads@gmail.com>
2024-03-08 11:15:30 +0000

    last commit of the day

---
+++ sync_test.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+# Return a string representing the current working directory.
+# os.getcwd()
+# r -> to ignore the back slash as a command
+
+path_source = r"C:\\Users\\leona\\Desktop\\test\\source\\"
+path_replica = r"C:\\Users\\leona\\Desktop\\test\\replica\\"
+
+# path_folders = os.path.dirname(path_main)
+
+# replace
+
+dir_sync = os.listdir(path_source)
+
+for file in dir_sync:
+    print(file, type(file))
+    try:
+        os.rename(path_replica + '\\' + file, path_source + '\\' + file)
+    except:
+        print('File or Folder: not found!')
+
+
+'''
+print('-' * 15)
+
+print(path_source, type(path_source))
+
+print('-' * 15)
+
+print(len(dir_sync))'''
